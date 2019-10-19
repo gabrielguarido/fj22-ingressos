59a81880f96bbb3635a090a8f917b209885b6ff8
Gabriel <gabriel@gmail.com>
2019-10-19 10:29:23 -0300

    Adicionando campo preço

--- src/main/java/br/com/caelum/ingresso/model/Filme.java
+++ src/main/java/br/com/caelum/ingresso/model/Filme.java
@@ -1,10 +1,13 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.Duration;
 
 /**
  * Created by nando on 03/03/17.
@@ -15,9 +18,14 @@ public class Filme {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	private String nome;
+
 	private Duration duracao;
+
 	private String genero;
+
+	private BigDecimal preco = BigDecimal.ZERO;
 
 	/**
 	 * @deprecated hibernate only
@@ -26,10 +34,11 @@ public class Filme {
 
 	}
 
-	public Filme(String nome, Duration duracao, String genero) {
+	public Filme(String nome, Duration duracao, String genero, BigDecimal preco) {
 		this.nome = nome;
 		this.duracao = duracao;
 		this.genero = genero;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -62,6 +71,14 @@ public class Filme {
 
 	public void setGenero(String genero) {
 		this.genero = genero;
+	}
+
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
 
 }
